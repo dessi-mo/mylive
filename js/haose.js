6382cff2e2559c621359afb722c2add789ea5cb2
dessi-mo <67297736+dessi-mo@users.noreply.github.com>
2023-12-27 00:50:57 +0800

    Update haose.js

--- js/haose.js
+++ js/haose.js
@@ -1,1 +1,67 @@
-1
+    {
+    
+    "站名":"好色TV",
+    
+    "主页url":"https://hsex.icu",
+    
+    "简介":"video-info-item video-info-content vod_content\"&&</div>",
+    
+    "导演":"导演：&&</div>",
+    
+    "主演":"主演：&&</div>",
+    
+    "影片类型":"video-info-aux scroll-content\">&&立即播放",
+    
+    "影片状态":"集数：&&</div>",
+    
+    "数组":"col-xs-6 col-md-3\">&&class=\"info\">",
+    
+    "副标题":"duration\">&&</var>",
+    
+    "链接":"href=\"&&\"",
+    
+    "标题":"title=\"&&\"",
+    
+    "图片":"background-image: url('&&')",
+      
+    "搜索url":"/search-{pg}.htm?search={wd}&sort=new", //https://hsex.icu/search-2.htm?search=黑丝&sort=new
+    
+    "搜索模式":"1",
+    
+    "搜索数组":"col-xs-6 col-md-3\">&&class=\"info\">",
+    
+    "搜索图片":"background-image: url('&&')",
+    
+    "搜索标题":"title=\"&&\"",
+    
+    "搜索链接":"href=\"&&\"",
+    
+    "搜索副标题":"duration\">&&</var>",
+    
+//    "搜索后缀":"/video/",
+    
+//    "线路数组":"data-dropdown-value=&&</div>",
+    
+//    "线路标题":"<span>&&</small>[替换:</span><small>>>>祥子影视共]+集",
+    
+//    "播放数组":"<div class=\"sort-item\"&&</div>",
+    
+//    "导演":"导演：&&</div>",
+    
+//    "主演":"主演：&&</div>",
+    
+//    "简介":"<div class=\"video-info-item video-info-content vod_content\">&&</div>",
+    
+//    "影片类型":"icon-cate-ds\"&&立即播放",
+    
+//    "影片状态":"集数：&&</div>",
+    
+//    "播放列表":"<a&&</a>",
+    
+//    "播放标题":"<span>&&</span>",
+    
+//    "播放链接":"href=\"&&\"",
+      
+    "分类url":"https://hsex.icu/{cateId}list-{catePg}.htm;;ak", //https://hsex.icu/list-1.htm
+      
+    "分类":"全部$#周榜$top7_#月榜$top_#5分钟以上$5min_#10分钟以上$10min_"}
