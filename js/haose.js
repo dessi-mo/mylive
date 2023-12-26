d4bfc7aae760fbcbc13927f63d332a8fea50f31b
dessi-mo <67297736+dessi-mo@users.noreply.github.com>
2023-12-27 01:11:18 +0800

    Update haose.js

--- js/haose.js
+++ js/haose.js
@@ -1,43 +1,31 @@
-    {
-    
+{
     "站名":"好色TV",
-    
     "主页url":"https://hsex.icu",
-    
+    "分类url":"https://hsex.icu/{cateId}-1.htm;;ak",
+    "分类":"全部$list#周榜$top7_list#月榜$top_list#5分钟以上$5min_list#10分钟以上$long_list",
+        
+    "请求头": "User-Agent$Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36",
+
     "简介":"video-info-item video-info-content vod_content\"&&</div>",
-    
     "导演":"导演：&&</div>",
-    
     "主演":"主演：&&</div>",
-    
     "影片类型":"video-info-aux scroll-content\">&&立即播放",
-    
     "影片状态":"集数：&&</div>",
     
     "数组":"col-xs-6 col-md-3\">&&class=\"info\">",
-    
-    "副标题":"duration\">&&</var>",
-    
-    "链接":"href=\"&&\"",
-    
     "标题":"title=\"&&\"",
-    
+    "副标题":"duration\">&&</var>",
     "图片":"background-image: url('&&')",
+    "链接":"href=\"&&\"",
       
     "搜索url":"/search-{pg}.htm?search={wd}&sort=new", //https://hsex.icu/search-2.htm?search=黑丝&sort=new
-    
     "搜索模式":"1",
-    
     "搜索数组":"col-xs-6 col-md-3\">&&class=\"info\">",
-    
-    "搜索图片":"background-image: url('&&')",
-    
     "搜索标题":"title=\"&&\"",
-    
-    "搜索链接":"href=\"&&\"",
-    
     "搜索副标题":"duration\">&&</var>",
-    "播放链接": "解析+\"url\"*\"&&\"",
-    "分类url":"https://hsex.icu/{cateId}-1.htm;;ak",
-      
-    "分类":"全部$list#周榜$top7_list#月榜$top_list#5分钟以上$5min_list#10分钟以上$10min_list"}
+    "搜索图片":"background-image: url('&&')",
+    "搜索链接":"href=\"&&\"",
+
+        
+    "播放链接": "<source src=\"&&\""
+}
