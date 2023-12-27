53da3b7a08d447c3bc7bd5e1229b19cab5a2fab2
dessi-mo <67297736+dessi-mo@users.noreply.github.com>
2023-12-27 22:41:37 +0800

    Update 直播秀.js

--- js/直播秀.js
+++ js/直播秀.js
@@ -4,18 +4,18 @@
 "直接播放":"1",
 "首页": "http://api.hclyz.com:81/mf/json.txt",
 "分类url":"http://api.hclyz.com:81/mf/json.txt",
-"分类":"直播秀$jsonweishizhibo.txt",
+//"分类":"直播秀$jsonweishizhibo.txt",
         
 //"分类":"卫视直播$jsonweishizhibo.txt#龙珠直播$jsonlongzhu.txt#映客直播$jsonyingke.txt#卡哇伊直播$jsonkawayi.txt",
-//"请求头": "User-Agent$Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36",
-//"分类数组": "{ \"&&}",
-//"分类标题": "title\":\"&&\"",
-//"分类ID": "address\":\"&&\"",
+"请求头": "User-Agent$Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36",
+"分类数组": "{ \"&&}",
+"分类标题": "title\":\"&&\"",
+"分类ID": "address\":\"&&\"",
 
 
 "数组":"{ \"&&}",
 "标题":"title\":\"&&\"",
-"图片":"xinimg\":\"&&\"",
+"图片":"img\":\"&&\"",
 "链接前缀":"http://api.hclyz.com:81/mf/",
 "链接":"address\":\"&&\"",
 
