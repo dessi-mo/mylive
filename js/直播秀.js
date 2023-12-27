88bf1d18625d07c5480bb00ef9e48defff0efc68
dessi-mo <67297736+dessi-mo@users.noreply.github.com>
2023-12-27 21:27:25 +0800

    Update 直播秀.js

--- js/直播秀.js
+++ js/直播秀.js
@@ -5,19 +5,20 @@
 "首页": "http://api.hclyz.com:81/mf/json.txt",
 "分类url":"http://api.hclyz.com:81/mf/{cateId}",
 //"分类":"卫视直播$jsonweishizhibo.txt#龙珠直播$jsonlongzhu.txt#映客直播$jsonyingke.txt#卡哇伊直播$jsonkawayi.txt",
-"请求头": "User-Agent$Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36",
+
 "分类数组": "{ \"&&}",
 "分类标题": "title\":\"&&\"",
 "分类ID": "address\":\"&&\"",
-"图片代理": "1",
-"调试":"1",
 
+"调试":"1",
+"图片代理": "1",
+        
 "数组":"{ \"&&}",
 "标题":"title\":\"&&\"",
 "图片":"img\":\"&&\"",
 //"链接":"address\":\"&&\"",
 "播放链接": "address\":\"&&\"",
-"直接播放":"0",
+"直接播放":"1",
 "免嗅": "1", 
 //"嗅探词":"flv#m3u8"
 
