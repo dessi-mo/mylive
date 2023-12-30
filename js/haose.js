7c359880e291a8c148d2ee23026cf70cc9f04b99
dessi-mo <67297736+dessi-mo@users.noreply.github.com>
2023-12-30 22:02:59 +0800

    Update haose.js

--- js/haose.js
+++ js/haose.js
@@ -5,7 +5,7 @@
     "分类url":"https://hsex.icu/{cateId}-{catePg}.htm[https://hsex.icu/{cateId}-1.htm]",
     "分类":"全部$list#周榜$top7_list#月榜$top_list#5分钟以上$5min_list#10分钟以上$long_list",
         
-//    "请求头": "User-Agent$Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36",
+    "请求头": "User-Agent$Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36",
 //    "简介":"video-info-item video-info-content vod_content\"&&</div>",
 //    "导演":"导演：&&</div>",
 //    "主演":"主演：&&</div>",
