4b864905315de096cf2bef863aeb1c3a86cd42d2
dessi-mo <67297736+dessi-mo@users.noreply.github.com>
2024-01-15 00:11:22 +0800

    Update 测试模板.js

--- js/测试模板.js
+++ js/测试模板.js
@@ -1,17 +1,17 @@
-var rule={
-    title:'188影视',
+var rule = {
+    title:'18av',
     host:'https://hsex.icu',
-	url:'/fyclass-fypage.htm',//网站的分类页面链接
-	filterable:0,//是否启用分类筛选,
-    searchUrl:'/search-fypage.htm?search=**&sort=new',//搜索链接 可以是完整路径或者相对路径,用于分类获取和推荐获取 **代表搜索词 fypage代表页数
-    searchable:2,
-    headers:{'User-Agent':'PC', },
+    url:'/fyclass-fypage.htm',
+    headers:{
+        'User-Agent':'MOBILE_UA'
+    },
+    timeout:5000,
     class_name:'全部&周榜&月榜&5分钟以上&10分钟以上',//静态分类名称拼接
     class_url:'list&top7_list&top_list&5min_list&long_list',//静态分类标识拼接
-    推荐:'.col-sm-6;h5&&Text;img&&data-src;var&&Text;a&&href',
-    double:false, // 推荐内容是否双层定位
+
+    limit:5,
+    play_parse:true,
+    lazy:'',
     一级:'.col-sm-6;h5&&Text;img&&data-src;var&&Text;a&&href',
     二级:*,
-    搜索:'.col-sm-6;h5&&Text;img&&data-src;var&&Text;a&&href',
 }
-
