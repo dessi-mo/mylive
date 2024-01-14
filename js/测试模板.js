16af18a88b713fed04546fbbe652c89293c53303
dessi-mo <67297736+dessi-mo@users.noreply.github.com>
2024-01-15 00:30:32 +0800

    Update 测试模板.js

--- js/测试模板.js
+++ js/测试模板.js
@@ -1,17 +1,23 @@
 var rule = {
-    title:'18av',
+    title:'小品网',
     host:'https://hsex.icu',
     url:'/fyclass-fypage.htm',
+    searchUrl:'/search.php?q=**',
+    searchable:2,
+    quickSearch:0,
+    filterable:0,
     headers:{
-        'User-Agent':'MOBILE_UA'
+        'User-Agent':'PC_UA'
     },
     timeout:5000,
     class_name:'全部&周榜&月榜&5分钟以上&10分钟以上',//静态分类名称拼接
     class_url:'list&top7_list&top_list&5min_list&long_list',//静态分类标识拼接
-
-    limit:5,
     play_parse:true,
     lazy:'',
+    limit:6,
+    double:true,
+    推荐:'.col-sm-6;h5&&Text;img&&data-src;var&&Text;a&&href',
     一级:'.col-sm-6;h5&&Text;img&&data-src;var&&Text;a&&href',
     二级:'*',
+    搜索:'*',
 }
